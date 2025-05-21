9e9dbefde55776e759bd3e1f2e87ce63ce099df5
vivanjoao <joaopedrovivan@gmail.com>
2025-05-20 21:25:44 -0300

    Tested List functions

---
+++ build/CMakeFiles/ByteOS.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "ByteOS"
+  "ByteOS.pdb"
+  "CMakeFiles/ByteOS.dir/main.c.o"
+  "CMakeFiles/ByteOS.dir/main.c.o.d"
+  "CMakeFiles/ByteOS.dir/utils/list.c.o"
+  "CMakeFiles/ByteOS.dir/utils/list.c.o.d"
+  "CMakeFiles/ByteOS.dir/utils/utils.c.o"
+  "CMakeFiles/ByteOS.dir/utils/utils.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ByteOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ByteOS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ByteOS.
+# This may be replaced when dependencies are built.
