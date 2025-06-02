328f2b0ce470106cb52d6125d4f42e50c95445eb
vivanjoao <joaopedrovivan@gmail.com>
2025-06-02 13:27:29 -0300

    Created Branch

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 #include "utils/utils.h"
 #include "utils/list.h"
 
-void debugList(List *list, void (*printFunction)(void *value)){
-    Node *aux = list->head;
-    while (aux){
-        printFunction(aux->info);
-        aux = aux->next;
-    }
-}
-
 int main(){
 
+    return 0;
 }
